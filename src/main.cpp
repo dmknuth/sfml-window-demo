24479819479fa2d0ea91056f2ce02af6e5e7549b
David Knuth <david_knuth@icloud.com>
2025-08-23 08:30:57 -0400

    Add command line args for ball couunt and ball radius.

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,25 @@ process_args
 (
     int         argc,
     char*       argv[],
-    u_int16_t&  x,
-    u_int16_t&  y,
-    float&      anti_alias
+    u_int16_t&  size_x,
+    u_int16_t&  size_y,
+    float&      anti_alias,
+    u_int16_t&  count,
+    u_int16_t&  radius
 )
 {
     switch (argc)
     {
+        case 6:
+            radius = std::atoi(argv[5]);
+        case 5:
+            count = std::atoi(argv[4]);
         case 4:
             anti_alias = std::strtof(argv[3], 0);
         case 3:
-            y = std::atoi(argv[2]);
+            size_y = std::atoi(argv[2]);
         case 2:
-            x = std::atoi(argv[1]);
+            size_x = std::atoi(argv[1]);
             break;
         default:
             break;
@@ -34,8 +40,10 @@ int main(int argc, char* argv[])
     u_int16_t   x = 640;
     u_int16_t   y = 480;
     float       anti_alias = 1.0;
+    u_int16_t   count = 1;
+    u_int16_t   radius = 20;
 
-    if(process_args(argc, argv, x, y, anti_alias) == true)
+    if(process_args(argc, argv, x, y, anti_alias, count, radius) == true)
     {
     
         Window* window = new Window();
@@ -46,8 +54,8 @@ int main(int argc, char* argv[])
             std::cout << "ball_machine is NULL" << std::endl;
             return -1;
         }
-        ball_machine -> set_size(40);
-        ball_machine -> set_count(20);
+        ball_machine -> set_size(radius);
+        ball_machine -> set_count(count);
         ball_machine -> set_boundaries(x, y);
         ball_machine -> generate();
 
