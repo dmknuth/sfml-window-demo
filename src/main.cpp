1addcd3ca21c05df9c98fca660ad66fcaf421045
David Knuth <david_knuth@icloud.com>
2025-08-31 21:54:41 -0400

    Create a struct to contain all command line args.

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,22 @@
 #include <iostream>
 #include <unistd.h>
 
-const   u_int16_t   k_x = 640;
-const   u_int16_t   k_y = 480;
+const   u_int16_t   k_width = 640;
+const   u_int16_t   k_height = 480;
 const   float       k_anti_alias = 1.1;
 const   u_int16_t   k_count = 1;
 const   u_int16_t   k_radius = 20;
 const   bool        k_grid = false;
+
+struct arg_struct
+{
+    u_int16_t   width;
+    u_int16_t   height;
+    float       anti_alias;
+    u_int16_t   count;
+    u_int16_t   radius;
+    bool        grid;
+};
 
 //----------------------------------------------------------------------------------------
 // Return true to continue running the application.
@@ -17,14 +27,9 @@ const   bool        k_grid = false;
 bool
 process_args
 (
-    int         argc,
-    char*       argv[],
-    u_int16_t&  size_x,
-    u_int16_t&  size_y,
-    float&      anti_alias,
-    u_int16_t&  count,
-    u_int16_t&  radius,
-    bool&       grid
+    int                 argc,
+    char*               argv[],
+    struct arg_struct*  arg_list
 )
 {
     for(;;)
@@ -32,21 +37,21 @@ process_args
         switch(getopt(argc, argv, "a:c:ghr:vx:y:"))
         {
             case 'a':
-              anti_alias = std::strtof(optarg, 0);
+              arg_list -> anti_alias = std::strtof(optarg, 0);
 //              std::cout << "anti_alias " << anti_alias << std::endl;
               continue;
         
             case 'c':
-              count = std::atoi(optarg);
+              arg_list -> count = std::atoi(optarg);
 //              std::cout << "count " << count << std::endl;
               continue;
         
             case 'g':
-              grid = true;
+              arg_list -> grid = true;
               continue;
         
             case 'r':
-              radius = std::atoi(optarg);
+              arg_list -> radius = std::atoi(optarg);
 //              std::cout << "radius " << radius << std::endl;
               continue;
         
@@ -58,12 +63,12 @@ process_args
               }
         
             case 'x':
-              size_x = std::atoi(optarg);
+              arg_list -> width = std::atoi(optarg);
 //              std::cout << "size_x " << size_x << std::endl;
               continue;
         
             case 'y':
-              size_y = std::atoi(optarg);
+              arg_list -> height = std::atoi(optarg);
 //              std::cout << "size_y " << size_y << std::endl;
               continue;
         
@@ -77,8 +82,8 @@ process_args
                 std::cout << "-h or any other character to display this message." << std::endl;
                 std::cout << "-r set the radius of the objects. The default is " << k_radius << "." << std::endl;
                 std::cout << "-v display version of this application." << std::endl;
-                std::cout << "-x set the width of the window to create. The default is " << k_x << "." << std::endl;
-                std::cout << "-y set the height of the window to create. The default is " << k_y << "." << std::endl;
+                std::cout << "-x set the width of the window to create. The default is " << k_width << "." << std::endl;
+                std::cout << "-y set the height of the window to create. The default is " << k_height << "." << std::endl;
             return false;
                 break;    
         
@@ -93,28 +98,29 @@ process_args
 //----------------------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
-    u_int16_t   x = k_x;
-    u_int16_t   y = k_y;
-    float       anti_alias = k_anti_alias;
-    u_int16_t   count = k_count;
-    u_int16_t   radius = k_radius;
-    bool        grid = k_grid;
+    struct arg_struct arg_list;
+    arg_list.width = k_width;
+    arg_list.height = k_height;
+    arg_list.anti_alias = k_anti_alias;
+    arg_list.count = k_count;
+    arg_list.radius = k_radius;
+    arg_list.grid = k_grid;
     
-    if(process_args(argc, argv, x, y, anti_alias, count, radius, grid) == true)
+    if(process_args(argc, argv, &arg_list) == true)
     {
        auto window = std::make_unique<Window>();
         if(window != nullptr)
         {
-            window -> configure(x, y, anti_alias, count, radius, grid) -> create();
+            window -> configure(arg_list.width, arg_list.height, arg_list.anti_alias, arg_list.count, arg_list.radius, arg_list.grid) -> create();
             Shapes* shape_vector = new Shapes();
             if(shape_vector == nullptr)
             {
                 std::cerr << "shape_vector could not be created" << std::endl;
                 return -1;
             }
-            shape_vector -> set_size(radius);
-            shape_vector -> set_count(count);
-            shape_vector -> set_boundaries(x, y);
+            shape_vector -> set_size(arg_list.radius);
+            shape_vector -> set_count(arg_list.count);
+            shape_vector -> set_boundaries(arg_list.width, arg_list.height);
             shape_vector -> generate();
             window -> add_content(shape_vector);
             window -> process_events();
